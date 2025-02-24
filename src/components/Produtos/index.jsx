e1a528b3d159c3dc6b7d3a82b4a39887be3fd990
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-24 16:46:44 -0300

    Consumindo um Contexto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "@/context/CarrinhoContexto";
 
 const Produtos = () => {
+  
+  const {carrinho, setCarrinho} = useContext (CarrinhoContext)
+  
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
