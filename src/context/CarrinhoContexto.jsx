18a0d008f789f477bd879db0643aeb2f60200435
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-24 16:13:57 -0300

    Criando um componente Context API

---
+++ src/context/CarrinhoContexto.jsx
@@ -0,0 +1,10 @@
+import { createContext } from "react";
+
+export const CarrinhoContext = createContext();
+
+export const CarrinhoProvider = ({ children }) => {
+  return 
+    <CarrinhoContext.Provider>
+        {children}
+    </CarrinhoContext.Provider>;
+};
