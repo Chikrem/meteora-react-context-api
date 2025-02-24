08407bb8595cd1cdd3b87f5605ce83b24218b1f1
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-24 19:19:23 -0300

    Utilizando o useReducer

--- src/context/CarrinhoContexto.jsx
+++ src/context/CarrinhoContexto.jsx
@@ -1,19 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
-CarrinhoContext.displayName = "Carrinho"
+CarrinhoContext.displayName = "Carrinho";
 
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, setCarrinho] = useState([]);
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
   const [valorTotalCarrinho, setValorTotalCarrinho] = useState(0);
 
   return (
-    <CarrinhoContext.Provider value={{ carrinho, setCarrinho, quantidadeProdutos, setQuantidadeProdutos, valorTotalCarrinho, setValorTotalCarrinho }}>
+    <CarrinhoContext.Provider
+      value={{
+        carrinho,
+        dispatch,
+        quantidadeProdutos,
+        valorTotalCarrinho,
+      }}
+    >
       {children}
     </CarrinhoContext.Provider>
   );
 };
-
-
