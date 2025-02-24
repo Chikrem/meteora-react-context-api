db552fa2ca663f3c2edae414d8f29b6cbd7a3660
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-24 17:13:00 -0300

    Criando removerProduto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -5,31 +5,50 @@ import Titulo from "@/components/Titulo";
 import { CarrinhoContext } from "@/context/CarrinhoContexto";
 
 const Produtos = () => {
-  
-  const {carrinho, setCarrinho} = useContext (CarrinhoContext)
-  
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
   function adicionarProduto(novoProduto) {
     const temOProduto = carrinho.some((itemDoCarrinho) => {
-        itemDoCarrinho.id === novoProduto.id
+      itemDoCarrinho.id === novoProduto.id;
     });
-    
+
     if (!temOProduto) {
-        novoProduto.quantidade = 1;
-        return setCarrinho((carrinhoAnterior) => [
-            ...carrinhoAnterior,
-            novoProduto,
-        ]);
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
     }
-    
+
     setCarrinho((carrinhoAnterior) =>
       carrinhoAnterior.map((itemDoCarrinho) => {
-          if (itemDoCarrinho.id === novoProduto.id)
-              itemDoCarrinho.quantidade += 1;
-              return itemDoCarrinho;
-          })
-      );
+        if (itemDoCarrinho.id === novoProduto.id)
+          itemDoCarrinho.quantidade += 1;
+        return itemDoCarrinho;
+      })
+    );
   }
 
+  function removerProduto(id) {
+    // encontra o produto em questão
+    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+    // verifica se a quantidade é igual a um. Isso significa que este é o último
+    // produto do tipo no carrinho
+    const ehOUltimo = produto.quantidade === 1;
+    // Com o if faz a verificação do último produto do tipo no carrinho e atualiza o estado do carrinho
+    if (ehOUltimo) {
+      return setCarrinho((carrinhoAnterior) =>
+        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
+      );
+    }
+    // Se não é o último produto do carrinho, só atualizamos a quantidade, removendo um item
+    setCarrinho((carrinhoAnterior) =>
+      carrinhoAnterior.map((itemDoCarrinho) => {
+        if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
+        return itemDoCarrinho;
+      })
+    );
+  }
 
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
@@ -40,6 +59,7 @@ const Produtos = () => {
             key={produto.id}
             {...produto}
             adicionarProduto={adicionarProduto}
+            removerProduto={removerProduto}
           />
         ))}
       </div>
