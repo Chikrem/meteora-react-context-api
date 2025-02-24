7ad52b8b9d7a2edc1381902a50bb85defb2f48a0
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2025-02-24 17:42:14 -0300

    Melhorando a lógica das funções adicionar e remover

--- src/context/CarrinhoContexto.jsx
+++ src/context/CarrinhoContexto.jsx
@@ -15,38 +15,6 @@ export const CarrinhoProvider = ({ children }) => {
 // const [quantidadeProdutos, setQuantidadeProdutos] = useState(0);
 // const [valorTotalCarrinho, setValorTotalCarrinho] = useState(0);
 
-// function mudarQuantidade(id, quantidade) {
-//   return carrinho.map((itemDoCarrinho) => {
-//     if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
-//     return itemDoCarrinho;
-//   });
-// }
-
-// function adicionarProduto(novoProduto) {
-//   const temOProduto = carrinho.some(
-//     (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
-//   );
-//   if (!temOProduto) {
-//     novoProduto.quantidade = 1;
-//     return setCarrinho((carrinhoAnterior) => [
-//       ...carrinhoAnterior,
-//       novoProduto,
-//     ]);
-//   }
-//   setCarrinho(mudarQuantidade(novoProduto.id, 1));
-// }
-
-// function removerProduto(id) {
-//   const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-//   const ehOUltimo = produto.quantidade === 1;
-//   if (ehOUltimo) {
-//     return setCarrinho((carrinhoAnterior) =>
-//       carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-//     );
-//   }
-//   setCarrinho(mudarQuantidade(id, -1));
-// }
-
 // function removerProdutoCarrinho(id) {
 //   const produto = carrinho.filter(
 //     (itemDoCarrinho) => itemDoCarrinho.id !== id
--- src/hooks/useCarrinhoContext.js
+++ src/hooks/useCarrinhoContext.js
@@ -2,57 +2,44 @@ import { useContext } from "react";
 import { CarrinhoContext } from "../context/CarrinhoContexto";
 
 export const useCarrinhoContext = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
 
-    const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  function mudarQuantidade(id, quantidade) {
+    return carrinho.map((itemDoCarrinho) => {
+      if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
+      return itemDoCarrinho;
+    });
+  }
 
-    function adicionarProduto(novoProduto) {
-      const temOProduto = carrinho.some((itemDoCarrinho) => {
-        itemDoCarrinho.id === novoProduto.id;
-      });
-  
-      if (!temOProduto) {
-        novoProduto.quantidade = 1;
-        return setCarrinho((carrinhoAnterior) => [
-          ...carrinhoAnterior,
-          novoProduto,
-        ]);
-      }
-  
-      setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.map((itemDoCarrinho) => {
-          if (itemDoCarrinho.id === novoProduto.id)
-            itemDoCarrinho.quantidade += 1;
-          return itemDoCarrinho;
-        })
+  function adicionarProduto(novoProduto) {
+    const temOProduto = carrinho.some(
+      (itemDoCarrinho) => itemDoCarrinho.id === novoProduto.id
+    );
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho((carrinhoAnterior) => [
+        ...carrinhoAnterior,
+        novoProduto,
+      ]);
+    }
+    setCarrinho(mudarQuantidade(novoProduto.id, 1));
+  }
+
+  function removerProduto(id) {
+    const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+    const ehOUltimo = produto.quantidade === 1;
+    if (ehOUltimo) {
+      return setCarrinho((carrinhoAnterior) =>
+        carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
       );
     }
-  
-    function removerProduto(id) {
-      // encontra o produto em questão
-      const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-      // verifica se a quantidade é igual a um. Isso significa que este é o último
-      // produto do tipo no carrinho
-      const ehOUltimo = produto.quantidade === 1;
-      // Com o if faz a verificação do último produto do tipo no carrinho e atualiza o estado do carrinho
-      if (ehOUltimo) {
-        return setCarrinho((carrinhoAnterior) =>
-          carrinhoAnterior.filter((itemDoCarrinho) => itemDoCarrinho.id !== id)
-        );
-      }
-      // Se não é o último produto do carrinho, só atualizamos a quantidade, removendo um item
-      setCarrinho((carrinhoAnterior) =>
-        carrinhoAnterior.map((itemDoCarrinho) => {
-          if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
-          return itemDoCarrinho;
-        })
-      );
-    }
+    setCarrinho(mudarQuantidade(id, -1));
+  }
 
-    return {
-        carrinho,
-        setCarrinho,
-        adicionarProduto,
-        removerProduto,
-    }
-
-}
+  return {
+    carrinho,
+    setCarrinho,
+    adicionarProduto,
+    removerProduto,
+  };
+};
